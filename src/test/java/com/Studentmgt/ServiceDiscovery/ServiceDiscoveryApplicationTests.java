4ba10b1afeb125be7b6a7e006bd877c2ac9586e1
Siva <lakshmi.kalagatla@gmail.com>
2022-04-20 15:17:23 -0400

    First change

---
+++ src/test/java/com/Studentmgt/ServiceDiscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Studentmgt.ServiceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
